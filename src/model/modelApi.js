2e1418cbcd35b4516639e14e14f575d640e8c4b5
Jacob Penney <jacobpenney@KS190203.local>
2019-08-26 12:43:40 -0700

    init

--- src/model/modelApi.js
+++ src/model/modelApi.js
@@ -1,11 +1,19 @@
 let model = {
-	count: 0
+	authenticated: false,
+	email: '',
+	password: '',
+	errors: [],
+	tab: 'analytics'
 }
 const subscribers = []
 
 const reducers = {
-	INCREMENT: (state, action) => ({...state, count: state.count+1}),
-	DECREMENT: (state, action) => ({...state, count: state.count-1})
+	SET_AUTHENTICATED: (state, action) => ({...state, authenticated: true}),
+	UNSET_AUTHENTICATED: (state, action) => ({...state, authenticated: false}),
+	SET_EMAIL: (state, action) => ({...state, email: action.email}),
+	SET_PASSWORD: (state, action) => ({...state, password: action.password}),
+	ADD_ERROR: (state, action) => ({...state, errors: [...state.errors, action.error]}),
+	DISMISS_ERROR: (state, action) => ({...state, errors: state.errors.filter(error => error.type !== action.error.type)})
 }
 
 const reduce = (state, action) => reducers[action.type] ? reducers[action.type](state, action) : state
--- src/systems/App.jsx
+++ src/systems/App.jsx
@@ -1,19 +1,198 @@
 import React from 'react';
+
+// usage: modelApi.dispatch(action)
 import modelApi from '../model/modelApi.js'
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props)
+
+		this.validateCredentials = () => new Promise((resolve, reject) => {
+			const errors = []
+
+			if(!this.props.email.length) {
+				errors.push({type: 'login_email', value: 'invalid_email'})
+			} 
+
+			if(!this.props.password.length) {
+				errors.push({type: 'login_password', value: 'invalid_password'})
+			}
+
+			if(errors.length) {
+				errors.forEach(err => modelApi.dispatch({type: 'ADD_ERROR', error: err}))
+				reject()
+			} else {
+				resolve()
+			}
+		})
+
+		this.login = () => {
+			modelApi.dispatch({type: 'SET_AUTHENTICATED'})
+		}
+	}
+
+	renderCard(content, styles) {
+		return (
+			<div style={styles} className="Card">{content}</div>
+		)
+	}
+
+	renderLogo() {
+
+	}
+
+	renderAppTitle() {
+
+	}
+
+	renderHeader() {
+		return (
+			<div id="Header">
+				{[
+					this.renderLogo(),
+					this.renderAppTitle(),
+					this.renderSettings()
+				]}
+			</div>
+		)
+	}
+
+	renderDataTable() {
+		return (
+			null
+		)
+	}
+
+	renderTabs() {
+		return (
+			null
+		)
+	}
+
+	renderAnalytics() {
+		return (
+			null
+		)
+	}
+
+	renderPatrons() {
+		return (
+			null
+		)
+	}
+
+	renderFacesCatalogue() {
+		return (
+			null
+		)
+	}
+
+	renderTransactions() {
+		return (
+			null
+		)
+	}
+
+	renderProfile() {
+		return (
+			null
+		)
+	}
+
+	renderSettings() {
+		return (
+			null
+		)
+	}
+
+	renderTabContent() {
+		const tab = this.props.tab
+		return (
+			tab === 'analytics' ? this.renderAnalytics()
+			: tab === 'patrons' ? this.renderPatrons()
+			: tab === 'faces' ? this.renderFacesCatalogue()
+			: tab === 'profile' ? this.renderProfile()
+			: null
+		)
+	}
+
+	renderContent() {
+		return (
+			<div id="Content">
+				{[
+					this.renderTabs(),
+					this.renderTabContent()
+				]}
+			</div>
+		)
+	}
+
+	renderLogin() {
+		const loignErrorTypes = ['login_email', 'login_password']
+		const loginErrors = this.props.errors.reduce((acc, err) => {
+			if(loignErrorTypes.includes(err.type)) {
+				acc[err.type] = err.value
+			}
+			return acc
+		}, {})
+
+		const loginContent = (
+			<div id="Login">
+				<form onSubmit={e => {
+					e.preventDefault()
+
+					// returns a promise, if resolved, the credentials are valid.
+					// Otherwise, they are invalid and should have triggered an error
+					this.validateCredentials()
+						.then(this.login)
+						.catch(() => console.log('Invalid login credentials'))
+				}}>
+					<h2>Login</h2>
+					<div>
+						<span>Email</span>
+						<input 
+							onFocus={e => modelApi.dispatch({type: 'DISMISS_ERROR', error: {type: 'login_email'}})} 
+							onChange={e => modelApi.dispatch({type: 'SET_EMAIL', email: e.target.value})}
+						/>
+						{
+							loginErrors.login_email 
+								? <span>ERROR</span>
+								: null
+						}
+					</div>
+					<div>
+						<span>Password</span>
+						<input 
+							onFocus={e => modelApi.dispatch({type: 'DISMISS_ERROR', error: {type: 'login_password'}})} 
+							onChange={e => modelApi.dispatch({type: 'SET_PASSWORD', password: e.target.value})}
+						/>
+						{
+							loginErrors.login_password
+								? <span>ERROR</span>
+								: null
+						}
+					</div>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		)
+
+		return this.renderCard(loginContent)
 	}
 
 	render() {
-		const model = this.props
+		const primaryContent = (
+			this.props.authenticated
+				? this.renderContent()
+				: this.renderLogin()
+		)
+
 		return (
-			<div style={{textAlign: 'center'}}>
-				<h1>Hello World</h1>
-				<div>Count: {model.count}</div>
-				<button onClick={e=>modelApi.dispatch({type: 'INCREMENT'})}>Inc</button>
-				<button onClick={e=>modelApi.dispatch({type: 'DECREMENT'})}>Dec</button>
+			<div id='App'>
+				{[
+					this.renderHeader(),
+					primaryContent
+				]}
 			</div>
 		)
 	}
